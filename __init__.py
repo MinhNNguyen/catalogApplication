e936bd046b7111282dd05b1a1e654972b2b4196a
Minh Nguyen <thanhnhatminhnguyen@gmail.com>
2019-01-11 18:40:35 -0800

    Resolving Database Issues 6

--- __init__.py
+++ __init__.py
@@ -283,10 +283,11 @@ def delete_item(item_name):
     if session['email'] != itemToDelete.creator_email:
       print('User is not authrozized to attempt this action')
       return redirect(url_for('home_page'))
-    print(itemToDelete)
+    output = itemToDelete
     data_session.delete(itemToDelete)
     data_session.commit()
-    return redirect(url_for('home_page'))
+    return output
+    # return redirect(url_for('home_page'))
   return render_template('delete_item.html', item = itemToDelete)
 
 
